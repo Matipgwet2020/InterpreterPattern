08e038972391ef4806940461c9af42d564592926
mmatipgwet@gmail.com <mmatipgwet@gmail.com>
2021-10-16 20:48:22 +0100

    create class InterpreterPatternDemo

--- src/interpreterpattern/main.java
+++ src/interpreterpattern/main.java
@@ -10,7 +10,29 @@ package interpreterpattern;
  * @author Mgms
  */
 public class main {
+    // Rule: Marcel and Simeon are male
+    public static Expression getMaleExpression() {
+        Expression marcel = new TerminalExpression("Marcel");
+        Expression simeon = new TerminalExpression("Siméon");
+        
+        return new OrExpression(marcel,simeon);
+    }
+    
+    // Rule : Vanessa is a married women
+    public static Expression getMarriedWomanExpression() {
+        Expression vanessa = new TerminalExpression("Vanessa");
+        Expression married = new TerminalExpression("Married");
+        
+        return new AndExpression(vanessa,married);
+    }
     public static void main(String [] args) {
+        Expression isMale = getMaleExpression();
+        Expression isMarriedWoman = getMarriedWomanExpression();
+        
+        System.out.println(" Marcel is male?" + isMale.interpret("Marcel"));
+        System.out.println("Vanessa is a married women?" + isMarriedWoman.interpret("Married Vanessa"));
+        
+        
         
     }
     
